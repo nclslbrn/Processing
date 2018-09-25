3c4d5f5c2462ae215b0b282651cf4e0860a97fed
nclslbrn <nclslbrn@gmail.com>
2018-09-25 14:56:24 +0200

    Remove libraries folder from tracking

--- libraries/PixelFlow/src/com/thomasdiewald/pixelflow/java/dwgl/DwGLRenderSettingsCallback.java
+++
@@ -1,7 +0,0 @@
-package com.thomasdiewald.pixelflow.java.dwgl;
-
-import com.thomasdiewald.pixelflow.java.DwPixelFlow;
-
-public interface DwGLRenderSettingsCallback{
-  void set(DwPixelFlow context, int x, int y, int w, int h);
-}
